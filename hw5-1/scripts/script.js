2b695f79977aac12f7cb9d83fc9a504e07c0ae8e
Daniel-Popov <daniepopov03@gmail.com>
2021-05-30 19:01:49 +0300

    Code refactoring;

--- hw5-1/scripts/script.js
+++ hw5-1/scripts/script.js
@@ -1,19 +1,24 @@
 var obstacle = prompt(
   "Kакъв е типът на обекта ,който се намира пред робота, стена, стол или нищо"
 );
+
 if (obstacle == "wall") {
   alert(" Go Sideway");
 }
+
 if (obstacle == "chair") {
   alert("Jump");
 }
+
 if (obstacle == "nothing") {
   alert("Forward");
 }
+
 var pixel = prompt("броя на пикселите в пространството");
 if (pixel % 2 == 0) {
   alert("мишката е засечена и робота се приготвя за атака");
 }
+
 var totalHits = 0;
 while (totalHits < 4) {
   var makeHit = confirm("Роботът,може ли да направи удър");
@@ -21,18 +26,19 @@ while (totalHits < 4) {
     totalHits++;
   }
 }
+
 var random = Math.floor(Math.random() * 10) + 1;
-{
-  if (random == 5) {
-    alert("oстана без мебели");
-  } else {
-    alert("размаза мишката");
-  }
+if (random == 5) {
+  alert("oстана без мебели");
+} else {
+  alert("размаза мишката");
 }
+
 var robotBattery = confirm("Имаш ли достатъчно енергиея за битка");
 if (robotBattery) {
   alert("продължавай да търсиш мишени");
 }
+
 if (!robotBattery) {
   alert("Ориентирайте се към най-близкия контакт");
   var firstNumber = Math.floor(Math.random() * 1000 + 1);
@@ -45,6 +51,7 @@ if (!robotBattery) {
     }
   }
 }
+
 var startNumber = 10;
 while (startNumber != 0) {
   if (startNumber % 2 == 0) {
